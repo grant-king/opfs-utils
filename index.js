e26b7ed3946144ee5b85962595811d01d67c1444
Grant King <grantryan.king@gmail.com>
2026-06-16 18:39:32 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+
+import {
+    get_opfs_info,
+    get_directory_handle,
+    get_file_handle,
+    store_text,
+    store_bytes,
+    get_opfs_contents_json,
+    copy_full_directory,
+} from './opfs_utils.js';
+
+export {
+    get_opfs_info,
+    get_directory_handle,
+    get_file_handle,
+    store_text,
+    store_bytes,
+    get_opfs_contents_json,
+    copy_full_directory,
+}
